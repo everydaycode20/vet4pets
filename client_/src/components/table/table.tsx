75a60730defe35aed72db571f56a764fa354951e
eddy <eddy.aguero@havasmagma.com>
2024-09-04 08:27:24 -0600

    update owners page

--- client_/src/components/table/table.tsx
+++ client_/src/components/table/table.tsx
@@ -1,37 +1,176 @@
-import { PaginationState, SortingState } from "@tanstack/react-table";
 import { useState } from "react";
+
+import {
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  PaginationState,
+  SortingState,
+  useReactTable,
+} from "@tanstack/react-table";
+import { TablePagination } from "@mui/base/TablePagination";
+
+import FirstPageRoundedIcon from "@mui/icons-material/FirstPageRounded";
+import LastPageRoundedIcon from "@mui/icons-material/LastPageRounded";
+import ChevronLeftRoundedIcon from "@mui/icons-material/ChevronLeftRounded";
+import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
+import ArrowDownwardOutlinedIcon from "@mui/icons-material/ArrowDownwardOutlined";
+
+import getSortingState from "../../utils/get-sorting-state";
+import JoinClasses from "../../utils/join-classes";
+
+import "./table.scss";
+import styles from "./table.module.scss";
 
 interface ITable {
   data: any[];
-  columns: { accessorKey: string; sort?: boolean; headerName: string }[];
-  pageSize?: number;
+  columns: any;
+  pagesSize?: number;
 }
 
-export default function Table({
-  data,
-  columns,
-  interface,
-  pageSize = 25,
-}: ITable) {
+export default function Table({ data, columns, pagesSize = 25 }: ITable) {
   const [sorting, setSorting] = useState<SortingState>([]);
 
   const [pagination, setPagination] = useState<PaginationState>({
     pageIndex: 0,
-    pageSize: pageSize,
+    pageSize: pagesSize,
   });
 
-  const tableCellsArray = [];
+  const table = useReactTable({
+    data,
+    debugTable: true,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onPaginationChange: setPagination,
+    onSortingChange: setSorting,
+    state: {
+      sorting,
+      pagination,
+    },
+  });
 
-  for (let i = 0; i < columns.length; i++) {
-    tableCellsArray.push({
-      accessorKey: columns[i].accessorKey,
-      cell: (info: any) => info.getValue(),
-      header: () => <span>{columns[i].headerName}</span>,
-      enableSorting: columns[i].sort === undefined ? true : columns[i].sort,
-    });
-  }
+  const { pageSize, pageIndex } = table.getState().pagination;
 
-//   console.log(<typeof data>);
+  return (
+    <table className="w-full max-h-full">
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th
+                key={header.id}
+                aria-sort={
+                  header.id === table.getState().sorting[0]?.id &&
+                  table.getState().sorting.length > 0
+                    ? getSortingState(table.getState().sorting)
+                    : "none"
+                }
+              >
+                {header.isPlaceholder ? null : (
+                  <div
+                    tabIndex={header.column.getCanSort() ? 0 : undefined}
+                    role={header.column.getCanSort() ? "button" : undefined}
+                    onClick={header.column.getToggleSortingHandler()}
+                    className={JoinClasses(
+                      "flex ",
+                      header.column.getCanSort()
+                        ? "cursor-pointer select-none"
+                        : ""
+                    )}
+                  >
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
 
-  return null;
+                    <span className="sr-only">
+                      {header.id === table.getState().sorting[0]?.id &&
+                      table.getState().sorting.length > 0
+                        ? "sorted " + getSortingState(table.getState().sorting)
+                        : "no sorted"}
+                    </span>
+
+                    {{
+                      asc: (
+                        <ArrowDownwardOutlinedIcon
+                          className="rotate-180"
+                          aria-hidden={true}
+                          focusable={false}
+                        />
+                      ),
+                      desc: (
+                        <ArrowDownwardOutlinedIcon
+                          className=""
+                          aria-hidden={true}
+                          focusable={false}
+                        />
+                      ),
+                    }[header.column.getIsSorted() as string] ?? (
+                      <div
+                        className={JoinClasses(
+                          "",
+                          styles["sorting-arrow-placeholder"]
+                        )}
+                      />
+                    )}
+                  </div>
+                )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+
+        <tr>
+          <TablePagination
+            count={table.getFilteredRowModel().rows.length}
+            rowsPerPage={25}
+            page={pageIndex}
+            onPageChange={(_, page) => {
+              table.setPageIndex(page);
+            }}
+            onRowsPerPageChange={(e) => {
+              const size = e.target.value ? Number(e.target.value) : 10;
+              table.setPageSize(size);
+            }}
+            rowsPerPageOptions={[]}
+            slotProps={{
+              actions: {
+                showFirstButton: true,
+                showLastButton: true,
+                slots: {
+                  firstPageIcon: () => (
+                    <FirstPageRoundedIcon htmlColor="#778CA2" />
+                  ),
+                  lastPageIcon: () => (
+                    <LastPageRoundedIcon htmlColor="#778CA2" />
+                  ),
+                  nextPageIcon: () => (
+                    <ChevronRightRoundedIcon htmlColor="#778CA2" />
+                  ),
+                  backPageIcon: () => (
+                    <ChevronLeftRoundedIcon htmlColor="#778CA2" />
+                  ),
+                },
+              },
+            }}
+          />
+        </tr>
+      </tbody>
+    </table>
+  );
 }
--- client_/src/routes/owners/owners.tsx
+++ client_/src/routes/owners/owners.tsx
@@ -1,25 +1,9 @@
-import { useState, useMemo } from "react";
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  PaginationState,
-  SortingState,
-  useReactTable,
-} from "@tanstack/react-table";
-import { TablePagination } from "@mui/base/TablePagination";
+import { useMemo } from "react";
+import { ColumnDef } from "@tanstack/react-table";
 
-import FirstPageRoundedIcon from "@mui/icons-material/FirstPageRounded";
-import LastPageRoundedIcon from "@mui/icons-material/LastPageRounded";
-import ChevronLeftRoundedIcon from "@mui/icons-material/ChevronLeftRounded";
-import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
 import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
 
-import ArrowDownwardOutlinedIcon from "@mui/icons-material/ArrowDownwardOutlined";
 import JoinClasses from "../../utils/join-classes";
-import getSortingState from "../../utils/get-sorting-state";
 
 import MockDataPerson from "../../assets/mock_data-person.json";
 
@@ -38,15 +22,6 @@ interface Person {
 const defaultData: Person[] = MockDataPerson;
 
 export default function Owner() {
-  const [data, _setData] = useState(() => [...defaultData]);
-
-  const [sorting, setSorting] = useState<SortingState>([]);
-
-  const [pagination, setPagination] = useState<PaginationState>({
-    pageIndex: 0,
-    pageSize: 25,
-  });
-
   const columns = useMemo<ColumnDef<Person>[]>(
     () => [
       {
@@ -80,67 +55,8 @@ export default function Owner() {
     []
   );
 
-  // const rerender = useReducer(() => ({}), {})[1];
-
-  const table = useReactTable({
-    data,
-    debugTable: true,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    onPaginationChange: setPagination,
-    onSortingChange: setSorting,
-    state: {
-      sorting,
-      pagination,
-    },
-  });
-
-  const { pageSize, pageIndex } = table.getState().pagination;
-
-  // console.log(table.getState().sorting);
-
-  interface Person {
-    firstName: string;
-    lastName: string;
-    phone: string;
-    address: string;
-    registerDate: string;
-  }
-
-
   return (
     <section className="h-full">
-      <Table
-        data={MockDataPerson}
-        columns={[
-          {
-            accessorKey: "firstName",
-            headerName: "First Name",
-          },
-          {
-            accessorKey: "lastName",
-            headerName: "Last Name",
-          },
-          {
-            accessorKey: "phone",
-            headerName: "Phone Name",
-            sort: false,
-          },
-          {
-            accessorKey: "address",
-            headerName: "Address",
-            sort: false,
-          },
-          {
-            accessorKey: "registerDate",
-            headerName: "Register Date",
-          },
-        ]}
-        
-      />
-
       <div
         className={JoinClasses(
           "flex justify-end",
@@ -156,124 +72,7 @@ export default function Owner() {
         </button>
       </div>
 
-      <table className="w-full max-h-full">
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th
-                  key={header.id}
-                  aria-sort={
-                    header.id === table.getState().sorting[0]?.id &&
-                    table.getState().sorting.length > 0
-                      ? getSortingState(table.getState().sorting)
-                      : "none"
-                  }
-                >
-                  {header.isPlaceholder ? null : (
-                    <div
-                      tabIndex={header.column.getCanSort() ? 0 : undefined}
-                      role={header.column.getCanSort() ? "button" : undefined}
-                      onClick={header.column.getToggleSortingHandler()}
-                      className={JoinClasses(
-                        "flex ",
-                        header.column.getCanSort()
-                          ? "cursor-pointer select-none"
-                          : ""
-                      )}
-                    >
-                      {flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-
-                      <span className="sr-only">
-                        {header.id === table.getState().sorting[0]?.id &&
-                        table.getState().sorting.length > 0
-                          ? "sorted " +
-                            getSortingState(table.getState().sorting)
-                          : "no sorted"}
-                      </span>
-
-                      {{
-                        asc: (
-                          <ArrowDownwardOutlinedIcon
-                            className="rotate-180"
-                            aria-hidden={true}
-                            focusable={false}
-                          />
-                        ),
-                        desc: (
-                          <ArrowDownwardOutlinedIcon
-                            className=""
-                            aria-hidden={true}
-                            focusable={false}
-                          />
-                        ),
-                      }[header.column.getIsSorted() as string] ?? (
-                        <div
-                          className={JoinClasses(
-                            "",
-                            styles["sorting-arrow-placeholder"]
-                          )}
-                        />
-                      )}
-                    </div>
-                  )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-
-          <tr>
-            <TablePagination
-              count={table.getFilteredRowModel().rows.length}
-              rowsPerPage={25}
-              page={pageIndex}
-              onPageChange={(_, page) => {
-                table.setPageIndex(page);
-              }}
-              onRowsPerPageChange={(e) => {
-                const size = e.target.value ? Number(e.target.value) : 10;
-                table.setPageSize(size);
-              }}
-              rowsPerPageOptions={[]}
-              slotProps={{
-                actions: {
-                  showFirstButton: true,
-                  showLastButton: true,
-                  slots: {
-                    firstPageIcon: () => (
-                      <FirstPageRoundedIcon htmlColor="#778CA2" />
-                    ),
-                    lastPageIcon: () => (
-                      <LastPageRoundedIcon htmlColor="#778CA2" />
-                    ),
-                    nextPageIcon: () => (
-                      <ChevronRightRoundedIcon htmlColor="#778CA2" />
-                    ),
-                    backPageIcon: () => (
-                      <ChevronLeftRoundedIcon htmlColor="#778CA2" />
-                    ),
-                  },
-                },
-              }}
-            />
-          </tr>
-        </tbody>
-      </table>
+      <Table data={defaultData} columns={columns} pagesSize={25} />
     </section>
   );
 }
