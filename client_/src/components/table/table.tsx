46100c328017ea672758d71726cff18aafbfab35
eddy <ed.ac.2@hotmail.com>
2024-09-04 21:49:05 -0600

    update table

--- client_/src/components/table/table.tsx
+++ client_/src/components/table/table.tsx
@@ -52,125 +52,129 @@ export default function Table({ data, columns, pagesSize = 25 }: ITable) {
     },
   });
 
+  console.log(columns);
+
   const { pageSize, pageIndex } = table.getState().pagination;
 
   return (
-    <table className="w-full max-h-full">
-      <thead>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
-              <th
-                key={header.id}
-                aria-sort={
-                  header.id === table.getState().sorting[0]?.id &&
-                  table.getState().sorting.length > 0
-                    ? getSortingState(table.getState().sorting)
-                    : "none"
-                }
-              >
-                {header.isPlaceholder ? null : (
-                  <div
-                    tabIndex={header.column.getCanSort() ? 0 : undefined}
-                    role={header.column.getCanSort() ? "button" : undefined}
-                    onClick={header.column.getToggleSortingHandler()}
-                    className={JoinClasses(
-                      "flex ",
-                      header.column.getCanSort()
-                        ? "cursor-pointer select-none"
-                        : ""
-                    )}
-                  >
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
+    <div>
+      <table className="w-full max-h-full">
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th
+                  key={header.id}
+                  aria-sort={
+                    header.id === table.getState().sorting[0]?.id &&
+                    table.getState().sorting.length > 0
+                      ? getSortingState(table.getState().sorting)
+                      : "none"
+                  }
+                >
+                  {header.isPlaceholder ? null : (
+                    <div
+                      tabIndex={header.column.getCanSort() ? 0 : undefined}
+                      role={header.column.getCanSort() ? "button" : undefined}
+                      onClick={header.column.getToggleSortingHandler()}
+                      className={JoinClasses(
+                        "flex ",
+                        header.column.getCanSort()
+                          ? "cursor-pointer select-none"
+                          : ""
+                      )}
+                    >
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
 
-                    <span className="sr-only">
-                      {header.id === table.getState().sorting[0]?.id &&
-                      table.getState().sorting.length > 0
-                        ? "sorted " + getSortingState(table.getState().sorting)
-                        : "no sorted"}
-                    </span>
+                      <span className="sr-only">
+                        {header.id === table.getState().sorting[0]?.id &&
+                        table.getState().sorting.length > 0
+                          ? "sorted " +
+                            getSortingState(table.getState().sorting)
+                          : "no sorted"}
+                      </span>
 
-                    {{
-                      asc: (
-                        <ArrowDownwardOutlinedIcon
-                          className="rotate-180"
-                          aria-hidden={true}
-                          focusable={false}
+                      {{
+                        asc: (
+                          <ArrowDownwardOutlinedIcon
+                            className="rotate-180"
+                            aria-hidden={true}
+                            focusable={false}
+                          />
+                        ),
+                        desc: (
+                          <ArrowDownwardOutlinedIcon
+                            className=""
+                            aria-hidden={true}
+                            focusable={false}
+                          />
+                        ),
+                      }[header.column.getIsSorted() as string] ?? (
+                        <div
+                          className={JoinClasses(
+                            "",
+                            styles["sorting-arrow-placeholder"]
+                          )}
                         />
-                      ),
-                      desc: (
-                        <ArrowDownwardOutlinedIcon
-                          className=""
-                          aria-hidden={true}
-                          focusable={false}
-                        />
-                      ),
-                    }[header.column.getIsSorted() as string] ?? (
-                      <div
-                        className={JoinClasses(
-                          "",
-                          styles["sorting-arrow-placeholder"]
-                        )}
-                      />
-                    )}
-                  </div>
-                )}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
+                      )}
+                    </div>
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
 
-      <tbody>
-        {table.getRowModel().rows.map((row) => (
-          <tr key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <td key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
-            ))}
-          </tr>
-        ))}
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
-        <tr>
-          <TablePagination
-            count={table.getFilteredRowModel().rows.length}
-            rowsPerPage={25}
-            page={pageIndex}
-            onPageChange={(_, page) => {
-              table.setPageIndex(page);
-            }}
-            onRowsPerPageChange={(e) => {
-              const size = e.target.value ? Number(e.target.value) : 10;
-              table.setPageSize(size);
-            }}
-            rowsPerPageOptions={[]}
-            slotProps={{
-              actions: {
-                showFirstButton: true,
-                showLastButton: true,
-                slots: {
-                  firstPageIcon: () => (
-                    <FirstPageRoundedIcon htmlColor="#778CA2" />
-                  ),
-                  lastPageIcon: () => (
-                    <LastPageRoundedIcon htmlColor="#778CA2" />
-                  ),
-                  nextPageIcon: () => (
-                    <ChevronRightRoundedIcon htmlColor="#778CA2" />
-                  ),
-                  backPageIcon: () => (
-                    <ChevronLeftRoundedIcon htmlColor="#778CA2" />
-                  ),
-                },
+      <div className="pagination-container" aria-label="pagination table">
+        <TablePagination
+          count={table.getFilteredRowModel().rows.length}
+          rowsPerPage={25}
+          page={pageIndex}
+          onPageChange={(_, page) => {
+            table.setPageIndex(page);
+          }}
+          onRowsPerPageChange={(e) => {
+            const size = e.target.value ? Number(e.target.value) : 10;
+            table.setPageSize(size);
+          }}
+          rowsPerPageOptions={[]}
+          slots={{ root: "div" }}
+          slotProps={{
+            actions: {
+              showFirstButton: true,
+              showLastButton: true,
+              slots: {
+                firstPageIcon: () => (
+                  <FirstPageRoundedIcon htmlColor="#778CA2" />
+                ),
+                lastPageIcon: () => <LastPageRoundedIcon htmlColor="#778CA2" />,
+                nextPageIcon: () => (
+                  <ChevronRightRoundedIcon htmlColor="#778CA2" />
+                ),
+                backPageIcon: () => (
+                  <ChevronLeftRoundedIcon htmlColor="#778CA2" />
+                ),
               },
-            }}
-          />
-        </tr>
-      </tbody>
-    </table>
+            },
+          }}
+        />
+      </div>
+    </div>
   );
 }
