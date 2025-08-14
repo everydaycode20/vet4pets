07b7dec2b17d40e93fb5a00565afe90d755c9085
eddy <ed.ac.2@hotmail.com>
2025-08-13 18:00:26 -0600

    api first commit

---
+++ API/Models/Appointments.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Models
+{
+    public class Appointments
+    {
+        public int id { get; set; }
+
+        public DateTime date { get; set; }
+
+        public int pet { get; set; }
+
+        public int owner { get; set; }
+
+        public int type { get; set; }
+    }
+}
